0965c3a01f4461d5af43b90f79dcb4751c62d1df
TusharVaid30 <tusharvaid30@gmail.com>
2022-09-27 22:34:18 +0530

    4

--- src/charchters.js
+++ src/charchters.js
@@ -7,7 +7,7 @@ export const Data = [
     img: "https://images.hdqwalls.com/download/viper-valorant-wa-240x320.jpg",
     Name: "Viper",
     Profile:
-      "The American chemist, Viper deploys an array of poisonous chemical devices to control the battlefield and cripple the enemy's vision."
+      "The American chemist, Viper deploys an array of poisonous chemical devices to control the battlefield and cripple the enemy's vision. If the toxins don't kill her prey, her mind games surely will."
   },
   //SecondPerson
   {
@@ -16,7 +16,7 @@ export const Data = [
       "https://images.hdqwalls.com/download/jett-valorant-4k-2020-dl-240x320.jpg",
     Name: "Jett",
     Profile:
-      "Representing her home country of South Korea, Jett's agile and evasive fighting style lets her take risks no one else can"
+      "Representing her home country of South Korea, Jett's agile and evasive fighting style lets her take risks no one else can. She runs circles around every skirmish, cutting enemies before they even know what hit them."
   },
   {
     id: 3,
@@ -24,6 +24,13 @@ export const Data = [
       "https://images.hdqwalls.com/download/reyna-valorant-4k-1k-240x320.jpg",
     Name: "Reyna",
     Profile:
-      "Forged in the heart of Mexico, Reyna dominates single combat, popping off with each kill she scores."
+      "Forged in the heart of Mexico, Reyna dominates single combat, popping off with each kill she scores. Her capability is only limited by her raw skill, making her highly dependent on performance."
+  },
+  {id :4,
+   img : 
+     "https://images.hdqwalls.com/download/sage-valorant-cl-240x400.jpg",
+     Name : "Sage",
+     Profile:"The bastion of China, Sage creates safety for herself and her team wherever she goes. Able to revive fallen friends and stave off aggressive pushes, she provides a calm center to a hellish fight."
   }
+
 ];
